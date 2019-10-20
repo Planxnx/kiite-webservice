6eaefed0cfa278954a0f817b17d0c63ce49e4ecc
Planxnx <60010465@kmitl.ac.th>
2019-10-20 21:40:26 +0700

    connected DB

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -4,24 +4,37 @@ const config = require('../config.json')
 const ExtractJwt = require("passport-jwt").ExtractJwt; //ใช้ในการ decode jwt ออกมา
 const JwtStrategy = require("passport-jwt").Strategy; //ใช้ในการประกาศ Strategy
 
+const userService = require('./../model/user/service')
+
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: config.secret, 
+    secretOrKey: config.secret,
 }
 
 const jwtAuthAdmin = new JwtStrategy(jwtOptions, (payload, done) => {
-    if (payload.role === "admin") done(null, true); 
-    else done(null, false);
+    userService.getByUsername(payload.username).then((user) => {
+        if (user.role != "admin") done(null, false)
+        else done(null, true)
+    }).catch(err => {
+        console.log(`JWT Auth ERROR : ${err}`);
+        done(null, true)
+    })
+
 });
 
 const jwtAuthUser = new JwtStrategy(jwtOptions, (payload, done) => {
-    if (payload.username === "planxthanee") done(null, true); //ตอนใชี้งานจริงต้องคิวรี่ username จาก database
-    else done(null, false);
+    userService.getByUsername(payload.username).then((user) => {
+        if (!user) done(null, false)
+        else done(null, true)
+    }).catch(err => {
+        console.log(`JWT Auth ERROR : ${err}`);
+        done(null, true)
+    })
+
 });
 
-passport.use('admin-rule',jwtAuthAdmin);
-passport.use('user-rule',jwtAuthUser);
-
+passport.use('admin-rule', jwtAuthAdmin);
+passport.use('user-rule', jwtAuthUser);
 
 const adminAuth = passport.authenticate("admin-rule", {
     session: false
