b4b720da6740fb22a3079016d46c6471d3425b2c
Planxnx <60010465@kmitl.ac.th>
2020-01-02 11:41:26 +0700

    disconnect from senntiment api

--- routes/timeline.js
+++ routes/timeline.js
@@ -44,45 +44,29 @@ router.post('/', JWTAuthMiddleware.userAuth, (req, res, next) => {
     let iconType = ["cat", "dog", "pig", "penguin", "tiger"]
     let randIcon = Math.floor(Math.random() * 5)
     let hexString = (0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
-    const myURL = new URL(`/General?text=${req.body.text}`, 'http://13.229.79.95:5000/')
-    http.get(myURL, (resp) => {
-        const { statusCode } = resp;
-        console.log(statusCode)
-        resp.setEncoding('utf8');
-        let rawData = '';
-        resp.on('data', (chunk) => { rawData += chunk; });
-        resp.on('end', () => {
-            try {
-                const parsedData = JSON.parse(rawData);
-                console.log(parsedData.data.mood)
-                timelineService.create({
-                    text: req.body.text,
-                    mood: parsedData.data.mood,
-                    createdBy: req.body.username,
-                    icon: {
-                        iconType: iconType[randIcon],
-                        iconColor: `#${hexString}`,
-                    }
-                }).then(() => {
-                    req.app.io.emit('timeline', {
-                        message: "got a new status"
-                    })
-                    res.json({
-                        status: 200,
-                        message: "create success",
-                        mood: parsedData.data.mood,
-                    })
-                }).catch(err => {
-                    res.status(400).json({
-                        status: 400,
-                        message: err
-                    });
-                })
-            } catch (e) {
-                console.error(e.message);
-            }
+    timelineService.create({
+        text: req.body.text,
+        mood: undefined,
+        createdBy: req.body.username,
+        icon: {
+            iconType: iconType[randIcon],
+            iconColor: `#${hexString}`,
+        }
+    }).then(() => {
+        req.app.io.emit('timeline', {
+            message: "got a new status"
         })
-    }) 
+        res.json({
+            status: 200,
+            message: "create success",
+            mood: undefined,
+        })
+    }).catch(err => {
+        res.status(400).json({
+            status: 400,
+            message: err
+        });
+    })
 });
 
 module.exports = router;
